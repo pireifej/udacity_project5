761564f15214fd325f90e80a62441deb04ebfe0a
Paul Ireifej <paul.ireifej@gmail.com>
2015-02-10 15:03:01 -0500

    add files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
+/*
+
+This file contains all of the code that does the main work.
+
+*/
+
 var client_ID = "I3ZEE1QKWPUD2F3ICVHWQJUVBRJP2MES3VRHORZ4EOJ0ZWAO";
 var client_secret = "0L1BPBPX2YOJK23RQEOXPEKIO2X11NBGY5W2FJGRCGUCZHWF";
-var URL = "https://api.foursquare.com/v2/venues/search?client_id=" + client_ID + "&client_secret=" + client_secret + "&v=20130815&ll=40.7,-74&query=sushi";
+var URL = "https://api.foursquare.com/v2/venues/search?client_id=" +
+			client_ID +
+			"&client_secret=" +
+			client_secret +
+			"&v=20130815&ll=40.7,-74&query=sushi";
 
 var wikiRequestTimeout = setTimeout(function() {
 	$wikiElem.text("failed to get wikipedia resources");
@@ -14,5 +24,65 @@ $.ajax({
     }
 });
 
+function loadData() {
 
-$("#mapDiv").append(googleMap);
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
+
+    // NY times articles
+	var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&api-key=393997003a2ed9f518a44b5c77316eaf:17:70996517';
+    $.getJSON(nytimesURL, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        var articles = data.response.docs;
+		var items = [];
+        $.each(articles, function(index) {
+                var article = articles[index];
+				$nytElem.append("<li class='article'>" + "<a href='" + article.web_url + "'>" + article.headline.main + "</a>" + "<p>" + article.snippet + "</p></li>" );
+            })
+    }).error(function(e) {
+         $nytElem.text("New York Times Article Could Not Be Loaded");
+    });;
+
+    // Wikipedia links
+	var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityStr + "&format=json&callback=wikiCallBack";
+
+	var wikiRequestTimeout = setTimeout(function() {
+	    $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+	$.ajax({
+        url: wikiUrl,
+		dataType: "jsonp",
+        success: function(response) {
+            var articleList = response[1];
+
+	        $.each(articleList, function(index) {
+                var articleStr = articleList[index];
+				var url = "http://en.wikipedia.org/wiki/" + articleStr;
+				$wikiElem.append("<li><a href='" + url + "'>" + articleStr + "</a></li>");
+            })
+            clearTimeout(wikiRequestTimeout);
+        }
+	});
+
+
+    return false;
+};
+
--- js/helper.js
+++ js/helper.js
@@ -5,144 +5,277 @@ This file contains all of the code running in the background that makes app.js p
 Paul Ireifej
 */
 
-var googleMap = '<div id="map"></div>';
-
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
-var map;    // declares a global map variable
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
-function initializeMap() {
+// declares a global map variable
+var map;
 
-  var locations;
+// list of all markers plotted in the map
+var allMarkers = [];
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
+// URL for Google marker image
+var markerURL = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|";
 
-  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-  // <div id="map">, which is appended as part of an exercise late in the course.
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+// default and selected pin images for map markers
+var point1 = new google.maps.Point(0,0);
+var point2 = new google.maps.Point(10, 34);
+var size = new google.maps.Size(21, 34);
+var defaultPin = new google.maps.MarkerImage(markerURL + "FE7569", size, point1, point2);
+var selectedPin = new google.maps.MarkerImage(markerURL + "FFFF00", size, point1, point2);
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
+// Foursquare.com API
+var client_ID = "I3ZEE1QKWPUD2F3ICVHWQJUVBRJP2MES3VRHORZ4EOJ0ZWAO";
+var client_secret = "0L1BPBPX2YOJK23RQEOXPEKIO2X11NBGY5W2FJGRCGUCZHWF";
+var URL = "https://api.foursquare.com/v2/venues/search?client_id=" +
+			client_ID +
+			"&client_secret=" +
+			client_secret +
+			"&v=20130815&ll=40.7,-74&query=sushi";
 
-    // initializes an empty array
-    var locations = [];
+var Location = function(data) {
+	var self = this;
 
-    // where I live
-    locations.push("Holmdel, NJ");
+	this.name = ko.observable(data.name);
+	this.street = ko.observable(data.street);
+	this.city = ko.observable(data.city);
+	this.state = ko.observable(data.state);
 
-    // where my parents live
-    locations.push("Newburgh, NY");
+	// Wikipedia links
+	this.wikiUrls = ko.observableArray([]);
+	this.articleStrs = ko.observableArray([]);
 
-    // where I work
-    locations.push("Middletown, NJ");
+	// NY times articles
+	this.nytimesUrls = ko.observableArray([]);
 
-    return locations;
-  }
+	// URL for Google API street view image
+	this.address = ko.computed(function() {
+		return this.street() + ' ' + this.city() + ', ' + this.state();
+	}, this);
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
+	this.imgSrc = ko.computed(function() {
+		var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
+		return streetViewUrl + this.city() + ', ' + this.state();
+	}, this);
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+	// Set Wikipedia links
+	this.setWikiUrls = ko.computed(function() {
+	    var wikiRequestUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.city() + "&format=json&callback=wikiCallBack";
+		var wikiRequestTimeout = setTimeout(function() {
+			return "failed to get wikipedia resources";
+		}, 8000);
 
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
+		$.ajax({
+			url: wikiRequestUrl,
+			dataType: "jsonp",
+			success: function(response) {
+				clearTimeout(wikiRequestTimeout);
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
+				var articleList = response[1];
+				$.each(articleList, function(index) {
+					var articleStr = articleList[index];
+					var url = "http://en.wikipedia.org/wiki/" + articleStr;
+					self.wikiUrls.push({myUrl:url, myArticleStr:articleStr});
+				});
+			}
+		});
+	},this);
 
-    // kicked off whenever a marker is selected
-    google.maps.event.addListener(marker, 'click', function() {
-            console.log("Marker Selected!");
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
-
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
-    for (var place in locations) {
-
-      // the search request object
-      var request = {
-        query: locations[place]
-      };
-
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
-    }
-  }
-
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
-
-  // locations is an array of location strings returned from locationFinder()
-  locations = locationFinder();
-
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
-
+    this.setNytimesUrls = ko.computed(function() {
+		var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + self.city() + '&api-key=393997003a2ed9f518a44b5c77316eaf:17:70996517';
+		$.getJSON(nytimesURL, function(data) {
+			var articles = data.response.docs;
+			var items = [];
+			$.each(articles, function(index) {
+				var article = articles[index];
+				self.nytimesUrls.push({myUrl:article.web_url, myArticleStr:article.headline.main});
+            })
+		}).error(function(e) {
+			return "New York Times Article Could Not Be Loaded";
+		});
+	}, this);
 }
 
+var initialLocations = [
+	new Location(
+	{
+		name: "My Home",
+		street: "2 Windsor Terrace",
+        city: "Holmdel",
+        state: "NJ"
+	}),
+	new Location(
+	{
+		name: "Parents' Home",
+		street: "3 On the Green",
+		city: "Newburgh",
+		state: "NY"
+	}),
+	new Location(
+	{
+		name: "Work",
+		street: "180 Park Ave",
+		city: "Florham Park",
+		state: "NJ"
+	}),
+	new Location(
+	{
+		name: "Research",
+		street: "4202 E Fowler Ave",
+		city: "Tampa",
+		state: "FL"
+	})
+];
+
+var Map = function() {
+    this.initializeMap = function() {
+		var mapOptions = {
+			disableDefaultUI: true
+		};
+
+	  // This next line makes `map` a new Google Map JavaScript Object and attaches it to
+	  // <div id="map">, which is appended as part of an exercise late in the course.
+	  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+	  /*
+	  createMapMarker(placeData) reads Google Places search results to create map pins.
+	  placeData is the object returned from search results containing information
+	  about a single location.
+	  */
+	  function createMapMarker(placeData) {
+
+	    // The next lines save location data from the search result object to local variables
+    	var lat = placeData.geometry.location.lat();  // latitude from the place service
+	    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    	var name = placeData.formatted_address;   // name of the place from the place service
+	    var bounds = window.mapBounds;            // current boundaries of the map window
+
+	    // marker is an object with additional data about the pin for a single location
+
+	    var marker = new google.maps.Marker({
+			map: map,
+			position: placeData.geometry.location,
+			title: name,
+			icon: defaultPin
+	    });
+		allMarkers.push(marker);
+
+	    // infoWindows are the little helper windows that open when you click
+    	// or hover over a pin on a map. They usually contain more information
+	    // about a location.
+	    var infoWindow = new google.maps.InfoWindow({
+			content: name
+		});
+
+		// kicked off whenever a marker is selected
+		google.maps.event.addListener(marker, 'click', function() {
+		// reset the currently selected marker
+		for (var i= 0; i < allMarkers.length; i++) {
+			allMarkers[i].setIcon(defaultPin);
+		}
+		// show the selected marker as such
+		this.setIcon(selectedPin);
+		// "map" the marker to the Location via the Title propery
+		var markerTitle = this.getTitle();
+		$.each(vm.locationList(), function(index, value) {
+				if (markerTitle.indexOf(value.street()) > -1) {
+					vm.currentLocation(vm.locationList()[index]);
+				}
+			});
+		});
+
+		// this is where the pin actually gets added to the map.
+		// bounds.extend() takes in a map location object
+		bounds.extend(new google.maps.LatLng(lat, lon));
+		// fit the map to the new marker
+		map.fitBounds(bounds);
+		// center the map
+		map.setCenter(bounds.getCenter());
+	}
+
+	/*
+	callback(results, status) makes sure the search returned results for a location.
+	If so, it creates a new map marker for that location.
+	*/
+	function callback(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			createMapMarker(results[0]);
+		}
+		// select initial marker once all markers are displayed
+		if (allMarkers.length == initialLocations.length) {
+			allMarkers[0].setIcon(selectedPin);
+		}
+	}
+
+	/*
+	pinPoster(locations) takes in the array of locations created by locationFinder()
+	and fires off Google place searches for each location
+	*/
+	function pinPoster() {
+		// creates a Google place search service object. PlacesService does the work of
+		// actually searching for location data.
+		var service = new google.maps.places.PlacesService(map);
+
+		// Iterates through the array of locations, creates a search object for each location
+		for (var location in initialLocations) {
+			// the search request object
+			var address = initialLocations[location].address();
+			var request = {
+				query: address
+			};
+
+			// Actually searches the Google Maps API for location data and runs the callback
+			// function with the search results after each search.
+			service.textSearch(request, callback);
+		}
+	}
+
+		// Sets the boundaries of the map based on pin locations
+		window.mapBounds = new google.maps.LatLngBounds();
+
+		// pinPoster(locations) creates pins on the map for each location in
+		// the locations array
+		pinPoster();
+	}
+}
+
+var ViewModel = function() {
+	var self = this;
+	this.locationList = ko.observableArray([]);
+	initialLocations.forEach(function(locationItem) {
+            self.locationList.push(locationItem);
+	});
+    this.currentLocation = ko.observable(this.locationList()[0]);
+	this.selectLocation = function() {
+		self.currentLocation(this);
+		// reset the currently selected marker
+		for (var i= 0; i < allMarkers.length; i++) {
+			// show the selected marker as such
+			if (allMarkers[i].getTitle().indexOf(this.street()) > -1) {
+				allMarkers[i].setIcon(selectedPin);
+				continue;
+			}
+			allMarkers[i].setIcon(defaultPin);
+		}
+	};
+}
+
+// Start here! initializeMap() is called when page is loaded.
+var myMap = new Map();
+
 // Calls the initializeMap() function when the page loads
-window.addEventListener('load', initializeMap);
+window.addEventListener('load', myMap.initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
 window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-  map.fitBounds(mapBounds);
+	// Make sure the map bounds get updated on page resize
+	map.fitBounds(mapBounds);
 });
+
+var vm = new ViewModel();
+
+ko.applyBindings(vm);
