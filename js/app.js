bcbc68001debb591df8d13a05411b942e659546e
Paul Ireifej <paul.ireifej@gmail.com>
2015-03-14 14:14:37 -0400

    change

--- js/app.js
+++ js/app.js
@@ -188,26 +188,25 @@ var LocationsMap = function() {
 			* or hover over a pin on a map. They usually contain more information
 			* about a location.
 		*/
- var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-     '</div>';
+		var contentString = "<div id='content'>"+
+			"<div id='locationDetail'>" +
+			"</div>";
+		$.each(initialLocations, function(index, value) {
+				if (name.indexOf(value.street()) > -1) {
+					var currLoc = value;
+					console.log(value.imgSrc());
+					console.log(value.wikiUrls());
+					console.log(value.articleStrs());
+					console.log(value.nytimesUrls());
+					contentString += "<p class='header'>Uluru</h1>" +
+						"<div id='bodyContent'>" +
+						"<p>Attribution: Uluru, <a href='https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194'>" +
+						"https://en.wikipedia.org/w/index.php?title=Uluru</a>" +
+						"(last visited June 22, 2009).</p>" +
+						"</div>" +
+				}
+		});
+		contentString += "</div>";
 
 		var infoWindow = new google.maps.InfoWindow({
 			content: contentString
